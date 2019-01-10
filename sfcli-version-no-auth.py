d8634bb8c3b88a22b8003905080fdd0506c3a7e2
Thomas Nielsen <tnielsen@stratasolar.com>
2019-01-09 19:33:45 -0500

    Pretty print versions from json

--- sfcli-version-no-auth.py
+++ sfcli-version-no-auth.py
@@ -1,5 +1,7 @@
 import http.client
 from config import cfg
+import json
+import pandas as pd
 
 host = cfg.sf_instance + ".salesforce.com"
 conn = http.client.HTTPSConnection(host)
@@ -9,3 +11,9 @@ r1 = conn.getresponse()
 print(r1.status, r1.reason)
 data1 = r1.read()
 print(data1)
+
+po = pd.read_json(data1)
+
+print(po)
+
+
